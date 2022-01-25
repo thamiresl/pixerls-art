6ae4f535d91bc1ec694107e48d7197770e07b2be
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-24 23:30:10 -0300

    requisito 10 e 11

--- script.js
+++ script.js
@@ -19,6 +19,38 @@ function limparPixel() {
 }
 botaoLimpar.addEventListener('click', limparPixel);
 
+function aumentarBoard(quadrado) {
+  const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.innerHTML = '';
+  for (let index = 0; index < quadrado; index += 1) {
+    const linha = document.createElement('section');
+    linha.className = 'line';
+    for (let index = 0; index < quadrado; index += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      linha.appendChild(pixel);
+    }
+    pixelBoard.appendChild(linha);
+  }
+}
+
+function tamanhoLimite() {
+  let valor = document.querySelector('#board-size').value;
+  if (valor === '') {
+    alert('Board inválido!');
+  }
+  if (valor < 5) {
+    valor = 5;
+  }
+  if (valor > 50) {
+    valor = 50;
+  }
+  aumentarBoard(valor);
+}
+
+const botaoAdicionar = document.querySelector('#generate-board');
+botaoAdicionar.addEventListener('click', tamanhoLimite);
+
 // Referencias:
 // https://stackoverflow.com/questions/1887104/how-to-get-the-background-color-of-an-html-element
 // https://stackoverflow.com/questions/9681601/how-can-i-count-the-number-of-elements-with-same-class 
